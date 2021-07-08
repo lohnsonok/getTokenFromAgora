1ed0580d6b208c5ca15e6505c969f0436749c706
f20190738 <f20190738@hyderabad.bits-pilani.ac.in>
2021-07-08 20:00:31 +0530

    implemented token

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // const RtcTokenBuilder = require('../AgoraTokenGenerator/src/RtcTokenBuilder').RtcTokenBuilder;
 // const RtcRole = require('../AgoraTokenGenerator/src/RtcTokenBuilder').Role;
 
+// const { RtcTokenBuilder, RtcRole } = require("agora-access-token");
+
 // const appID = 'f6a78f2a4c8f4cddbba5c9508b499645';
 // const appCertificate = '29cdd42f9e8944e9a0874c6da372ad0d';
 // var channelName = '';
@@ -29,9 +31,79 @@
 // app.listen('3000');
 
 
+// const express = require('express');
+// const app = express();
+// app.use(express.static('public'));
+// app.listen(process.env.PORT || 3000, () => {
+//     console.log('Application is started');
+// });
+
+// module.exports = {
+//     RtcTokenBuilder: require('./src/RtcTokenBuilder').RtcTokenBuilder,
+//     RtcRole: require('./src/RtcTokenBuilder').Role,
+// }
+
+
+// const {RtcTokenBuilder, RtcRole} = require('agora-access-token');
+// export const agoraTempToken = https.onRequest((req,res) => {
+//     const data = JSON.parse(req.body);
+//     console.log(data);
+//     console.log(req);
+//     const channelName = data.name;
+//     const batch = afs.batch();
+//     if(!channelName) {
+//         return res.status(400).json({'error': 'channel is required'}).send();
+//     }
+//     const token =RtcTokenBuilder.buildTokenWithAccount(appId, appCertificate, channelName, account, role);
+//     console.log("Token is " + token);
+//     return res.status(200).json({'status': 'success', 'token': token}).send();
+// }
+
+
 const express = require('express');
+const {RtcTokenBuilder, RtcRole} = require('agora-access-token');
+const { response } = require("express");
+const PORT = 3000;
+const APP_ID = 'f6a78f2a4c8f4cddbba5c9508b499645';
+const APP_CERTIFICATE = '29cdd42f9e8944e9a0874c6da372ad0d';
 const app = express();
-app.use(express.static('public'));
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Application is started');
+
+
+const nocache = (req, response, next) => {
+    response.header('Cache-Control','private, no-cache, no-store, must-revalidate');
+    response.header('Expires', '-1');
+    response.header('Pragma', 'no-cache');
+    next();
+};
+
+
+const generateAccessToken =(req, res) => { 
+    res.header('Access-Control-Allow-Origin', '*');
+    const channelName = req.query.channelName;
+    if(!channelName) {
+        return res.status(500).json({'error': 'channel is required'});  
+    }
+    let uid =req.query.uid;
+    if(!uid || uid == ''){
+        uid = 0;
+    }
+    let role =RtcRole.PUBLISHER;
+    let expireTime = req.query.expireTime;
+    if(!expireTime || expireTime == ''){
+        expireTime = 3600;
+    } else {
+        expireTime = parseInt(expireTime, 10);
+    }
+    const currentTime = Math.floor(Date.now / 1000);
+    const privilageExpireTime = currentTime + expireTime;
+    const token = RtcTokenBuilder.buildTokenWithUid(APP_ID, APP_CERTIFICATE, channelName, uid, role, privilageExpireTime);  
+    return res.json({'token': token});
+};
+
+
+app.get('/access_token', nocache, generateAccessToken);
+
+
+app.listen(PORT, () => {
+    console.log(`listening on port: ${PORT}`);
 });
